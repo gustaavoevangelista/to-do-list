47e2bcb5a130a96f40ebb33017c2eb240bd4df44
gustaavoevangelista <gustavo.eng.henrique@gmail.com>
2022-09-15 23:32:56 +0100

    applying logic

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # TO-DO LIST
     To-do list using React
+
+    []display the added task below input area
+    []update tasks
+    []delete tasks
--- src/App.js
+++ src/App.js
@@ -3,10 +3,31 @@ import './App.css'
 
 export default function App () {
 
-  function addTask(){
+  const [task, setTask] = useState('')
+/*   const [update, setUpdate] = useState('')
+  const [del, setDelete] = useState('') */
 
+  function addTask(){
+    return (
+      <div className="display-tasks">
+{/*       {task}
+      <button onClick={updateTask}>Update</button>  
+      <button onClick={deleteTask}>Delete</button>  not working*/}
+      </div>
+      
+    )
   }
 
+  function updateTask(){
+    alert('clicked')
+  }
+
+  function deleteTask(){
+    let confirmation = prompt('Are you sure you want to delete this task?')
+    if(confirmation === 'Yes'){
+      task.useState('')
+    }
+  }
 
   return(
     <div className="app">
@@ -18,16 +39,14 @@ export default function App () {
         <input 
         type="text" 
         placeholder="Groceries - Monday morning"
+        value={task}
+        onChange={(e) => setTask(e.target.value)}
+        id="task-input"
         />
         <button onClick={addTask}>Add</button>
-      
+
       </div>
 
-      <div className="display-tasks">
-        tasks added
-        <button>Update</button>  
-        <button>Delete</button>  
-      </div>
 
     </div>
   )
